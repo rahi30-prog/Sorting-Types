3e1c6deff7e60a86ab440e153f75b20c3e132b3e
Rahi <141012258+rahi30-prog@users.noreply.github.com>
2024-07-09 18:29:36 +0530

    Update test_Bubble.py
    1. Removed the 'setUp' method.
    2. Converted the data to more readable, i.e. removed the 'self.xxx'.

--- test_Bubble.py
+++ test_Bubble.py
@@ -2,69 +2,62 @@ import unittest
 from Bubble import BubbleSort
 from Selection import SelectionSort
 
+# Test cases
+cases = [
+    ([5, 4, 3, 2, 1, 0], [0, 1, 2, 3, 4, 5]),
+    ([1], [1]),
+    ([1, 2, 3, 4, 9], [1, 2, 3, 4, 9])
+]
 
-class Bubblesort(unittest.TestCase):
-    cases = [
-        ([5, 4, 3, 2, 1, 0], [0, 1, 2, 3, 4, 5]),
-        ([1],[1]),
-        ([1, 2, 3, 4, 9], [1, 2, 3, 4, 9])
-        ]
+# Test data
+nochange = [1, 2, 5, 10]
+descend = [10, 9, 8, 7]
+same = [8, 8, 8]
+single = [1]
+
+class TestSortingAlgorithms(unittest.TestCase):
     
-    def setUp(self):
-        self.nochange = [1, 2, 5, 10]
-        self.descend = [10, 9, 8, 7]
-        self.same = [8, 8, 8]
-        self.single = [1]
-
-
     def test_steps_BubbleSort(self):
-        for test, expectedResult in self.cases:
+        for test, expectedResult in cases:
             self.assertEqual(BubbleSort(test), expectedResult)
 
-    def test_nochange(self):    #defined a function
+    def test_nochange(self):
         nochange_steps = [1, 2, 5, 10]
-        self.assertEqual(BubbleSort(self.nochange), nochange_steps)  #used assertequal by linking the Bubblesort algorithm with cases written in the 'setUp' function.
+        self.assertEqual(BubbleSort(nochange), nochange_steps)
 
     def test_descend(self):
         descend_steps = [
             [10, 9, 8, 7], [9, 10, 8, 7], [9, 8, 10, 7], [9, 8, 7, 10],
             [8, 9, 7, 10], [8, 7, 9, 10], [7, 8, 9, 10]
-            ]
-        #start with the given list and then apply the logic of bubble sort later that step!
-        self.assertEqual(BubbleSort(self.descend,True), descend_steps)
+        ]
+        self.assertEqual(BubbleSort(descend, True), descend_steps)
 
-    def test_same(self):    
-        self.assertEqual(BubbleSort(self.same), [8, 8, 8])
+    def test_same(self):
+        self.assertEqual(BubbleSort(same), [8, 8, 8])
 
     def test_single(self):
-        self.assertEqual(BubbleSort(self.single), self.single)
+        self.assertEqual(BubbleSort(single), single)
 
     def test_steps_SelectionSort(self):
-        for test, expectedResult in self.cases:
+        for test, expectedResult in cases:
             self.assertEqual(SelectionSort(test), expectedResult)
-
 
     def test_NoChange(self):
         NoChange_steps = [1, 2, 5, 10]
-        self.assertEqual(SelectionSort(self.nochange), NoChange_steps)
+        self.assertEqual(SelectionSort(nochange), NoChange_steps)
 
     def test_Descend(self):
-        Descend_steps = [[10, 9, 8, 7],
-                        [7, 9, 8, 10],
-                        [7, 8, 9, 10],
-                        [7, 8, 9, 10]
-                        ]
-        self.assertEqual(SelectionSort(self.descend, True), Descend_steps)
-
+        Descend_steps = [
+            [10, 9, 8, 7], [7, 9, 8, 10],
+            [7, 8, 9, 10], [7, 8, 9, 10]
+        ]
+        self.assertEqual(SelectionSort(descend, True), Descend_steps)
 
     def test_Same(self):
-        Same = [[8, 8, 8]]
-        self.assertEqual(SelectionSort(self.same), [8, 8, 8])
+        self.assertEqual(SelectionSort(same), [8, 8, 8])
 
     def test_Single(self):
-        self.assertEqual(SelectionSort(self.single), self.single)
+        self.assertEqual(SelectionSort(single), single)
 
-
-
-if __name__ == '__main__':  #as we have imported another module
-    unittest.main()     #framework of unit test case, runs the test cases.
+if __name__ == '__main__':
+    unittest.main()
